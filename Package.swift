75b90bee54020cb5e1a60dd7efcfba0cf23970b4
Hidde van der Ploeg <hiddevanderploeg@me.com>
2023-07-20 14:52:45 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,10 +16,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ContentBlurView"),
-        .testTarget(
-            name: "ContentBlurViewTests",
-            dependencies: ["ContentBlurView"]),
+        .target(name: "ContentBlurView")
     ]
 )
